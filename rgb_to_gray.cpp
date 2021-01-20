7c48ce4d6702d300911d63cfd2f204f93a429dde
Muhammet Fatih POLAT <44674445+mfatihp@users.noreply.github.com>
2021-01-20 12:03:43 +0300

    Add files via upload

---
+++ rgb_to_gray.cpp
@@ -0,0 +1,41 @@
+#include "opencv2/imgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include <string>
+#include <iostream>
+
+
+
+int main()
+{
+    // Image location
+    std::string image_loc = "leaf.jpg";
+
+    // Target split matrix
+    cv::Mat bgr[3];
+
+    // Outputs
+    cv::Mat gray_av, gray_w;
+
+    // Read image
+    cv::Mat image = cv::imread(image_loc, cv::IMREAD_COLOR);
+
+    // Split channels
+    cv::split(image, bgr);
+
+    // Color Averages
+    gray_av = (bgr[0] + bgr[1] + bgr[2])/3;
+
+    // Weighted Colors
+    // 0.21 R + 0.72 G + 0.07 B
+    gray_w = (bgr[0]*0.07 + bgr[1]*0.72 + bgr[2]*0.21);
+
+    //Show all three images
+    cv::imshow("Original", image);
+    cv::imshow("Color Averages", gray_av);
+    cv::imshow("Weighted Colors", gray_w);
+
+    cv::waitKey(0);
+
+    return 0;
+}
